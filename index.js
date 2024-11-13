6fa8bdd0b7bbd4c531f1404dea2d7368f824bcad
saro-dev <codersaro@gmail.com>
2024-11-13 11:51:02 +0530

    added cron

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors'); 
+const cron = require('node-cron');
+const axios = require('axios');
 
 // Load environment variables
 dotenv.config();
@@ -37,4 +39,14 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Set up a cron job to ping the server every 13 minutes
+cron.schedule('*/13 * * * *', async () => {
+  try {
+    await axios.get('https://serviceprovidersback.onrender.com');
+    console.log('Pinged the server to keep it awake');
+  } catch (error) {
+    console.error('Error pinging the server:', error.message);
+  }
 });
